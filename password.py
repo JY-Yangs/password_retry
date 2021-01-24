3da1a7421748cacf2258e84b5b1c7c9372ec9d1a
JY-Yangs <wbhyex@gmail.com>
2021-01-25 00:19:50 +0800

    update comment when input_chance = 0

--- password.py
+++ password.py
@@ -7,10 +7,13 @@ password = 'a123456'
 input_chance = 3
 
 while input_chance > 0:
+	input_chance = input_chance - 1 #這一行可以寫在最前面, 讓它每進入一次迴圈就扣1
 	input_password = input('請輸入密碼: ')
 	if input_password == password:
 		print('登入成功!')
 		break #逃出迴圈
 	else:
-		input_chance = input_chance - 1
-		print('密碼錯誤! 還有', input_chance, '次機會!')
+		if input_chance > 0:
+			print('密碼錯誤! 還有', input_chance, '次機會!')
+		else: # 把 input_chance = 0 時印出其他的訊息
+			print('密碼錯誤! 沒機會嘗試了! 要被鎖帳號啦!')
