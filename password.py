8768a9b3bab62ced71bc745ad1f6f2cb774e5627
JY-Yangs <wbhyex@gmail.com>
2021-01-25 00:03:33 +0800

    change to while input_chance > 0:

--- password.py
+++ password.py
@@ -6,7 +6,7 @@
 password = 'a123456'
 input_chance = 3
 
-while True: #不一定要寫"while True", 也可寫"while input_chance > 0:"
+while input_chance > 0:
 	input_password = input('請輸入密碼: ')
 	if input_password == password:
 		print('登入成功!')
@@ -14,6 +14,3 @@ while True: #不一定要寫"while True", 也可寫"while input_chance > 0:"
 	else:
 		input_chance = input_chance - 1
 		print('密碼錯誤! 還有', input_chance, '次機會!')
-		if input_chance == 0: # 前面如果寫while input_chance > 0:可以不用寫這兩行, 因為條件不合後就部會在進入迴圈
-			break # 前面如果寫while input_chance > 0:可以不用寫這兩行, 因為條件不合後就部會在進入迴圈
-		
